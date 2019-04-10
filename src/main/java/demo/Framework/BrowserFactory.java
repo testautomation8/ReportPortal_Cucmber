eebfc6c37e16b9a3fd56043584f6f0f8ff93953a
testautomation8 <Voltas83>
2019-04-10 15:25:56 +0100

    Code added for saucelabs

--- src/main/java/demo/Framework/BrowserFactory.java
+++ src/main/java/demo/Framework/BrowserFactory.java
@@ -3,6 +3,11 @@ package demo.Framework;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class BrowserFactory {
 
@@ -41,6 +46,31 @@ public class BrowserFactory {
                 }
                 driver = new ChromeDriver(options);
                 break;
+
+            case "SAUCE_LABS":
+
+                String sauceUserName = "rahultripathi8";
+                String sauceAccessKey = "f19a69a7-55c8-4c70-aff7-c0483b0a9353";
+                String sauceURL = "http://" + sauceUserName + ":" + sauceAccessKey + "@ondemand.eu-central-1.saucelabs.com/wd/hub";
+                System.out.println("URL=" + sauceURL);
+                DesiredCapabilities capabilities = new DesiredCapabilities();
+
+                //set browser
+                capabilities.setCapability("browserName", "chrome");
+                //set operating system version
+                capabilities.setCapability("platform", "macOS 10.13");
+                //set the browser version to 11.1
+                capabilities.setCapability("version", "70");
+                //set the build name of the application
+                capabilities.setCapability("build", "Onboarding Sample App - Java");
+                //set your test case name so that it shows up in Sauce Labs
+                capabilities.setCapability("name", "1-first-test");
+                try {
+                    driver = new RemoteWebDriver(new URL(sauceURL), capabilities);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+
         }
 
         return driver;
--- src/test/java/demo/RunCucumberTest.java
+++ src/test/java/demo/RunCucumberTest.java
@@ -7,7 +7,9 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions
         (plugin = {"pretty",
-                   "html:target/cucumber-reports"})
+                   "html:target/cucumber-reports"
+                //,"com.epam.reportportal.cucumber.ScenarioReporter"
+        })
 public class RunCucumberTest {
 
 }
